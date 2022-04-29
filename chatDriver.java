23a19ea23603c592f1971f0eac43d34cdd05decb
Alif Elham <alifelham@gmail.com>
2022-04-29 15:32:16 +0600

    Fixing git connection

--- chatDriver.java
+++
@@ -1,8 +0,0 @@
-public class chatDriver {
-    public static void main(String[] args) {
-        chatThread m = new chatThread();
-        new chatT1(m);
-        new chatT2(m);
-        new chatT3(m);
-    }
-}
